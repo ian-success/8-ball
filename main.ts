7d1878c24d45ab8fa01bebbb11ea046aabf9d059
ian-success <112981850+ian-success@users.noreply.github.com>
2022-09-13 12:37:11 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onGesture(Gesture.Shake, function () {
+    RandomNumber = randint(-1, 1)
+    if (RandomNumber == -1) {
+        basic.showString("The answer is no")
+    }
+    if (RandomNumber == 0) {
+        basic.showString("Answer Unclear")
+    }
+    if (RandomNumber == 1) {
+        basic.showString("My senses say Yes")
+    }
+})
+let RandomNumber = 0
+basic.showString("Shake to ask the 8 ball")
+basic.forever(function () {
+	
+})
