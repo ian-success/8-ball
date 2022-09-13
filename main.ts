5d180aee9211a3eebcaa3491921fad168921fefb
ian-success <112981850+ian-success@users.noreply.github.com>
2022-09-13 12:51:51 -0600

    Everything

--- main.ts
+++ main.ts
@@ -1,20 +1,31 @@
 input.onButtonPressed(Button.A, function () {
+    RandomNumber = randint(0, 1)
+    if (RandomNumber == 0) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("Dare")
+    }
+    basic.pause(2000)
     basic.clearScreen()
 })
-input.onGesture(Gesture.Shake, function () {
-    RandomNumber = randint(-1, 1)
-    if (RandomNumber == -1) {
-        basic.showString("The answer is no")
-    }
+input.onButtonPressed(Button.B, function () {
+    RandomNumber = randint(0, 3)
     if (RandomNumber == 0) {
-        basic.showString("Answer Unclear")
+        basic.showArrow(ArrowNames.North)
     }
     if (RandomNumber == 1) {
-        basic.showString("My senses say Yes")
+        basic.showArrow(ArrowNames.East)
     }
+    if (RandomNumber == 2) {
+        basic.showArrow(ArrowNames.South)
+    } else {
+        basic.showArrow(ArrowNames.West)
+    }
+    basic.pause(2000)
+    basic.clearScreen()
 })
 let RandomNumber = 0
-basic.showString("Shake to ask the 8 ball")
+basic.showString("Truth or dare?")
 basic.forever(function () {
 	
 })
